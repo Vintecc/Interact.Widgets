95fc78f56f65456b603e6d8cf004e818b8ffd94f
Jasper Lievens <jasper.lievens@vintecc.com>
2023-10-13 13:58:36 +0200

    change first widget

--- src/MyFirstInteractWidget/component.tsx
+++ src/MyFirstInteractWidget/component.tsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { IBaseProps, IBaseActions } from "@vintecc/interact";
 
 /* component interface */
-export interface IMyFirstInteractWidget {
-    name : string
-    onClick : () => void
+export interface IMyFirstInteractWidget extends IBaseProps, IBaseActions {
+    name: string,
 }
 
 /* component react logic */
 export const MyFirstInteractWidget = React.forwardRef<any, IMyFirstInteractWidget>((props, ref) => {
-    return <div ref={ref} onClick = {() => {props.onClick; console.log("hi console")}}  >       
-         <h1>Header {props.name}</h1>
+    return <div
+        ref={ref}
+        style={{
+            zIndex: props.zIndex,
+            backgroundColor: props.backgroundColor,
+            color: props.color,
+            fontSize: props.fontSize
+        }}
+
+        // standard events from IBaseActions
+        onClick={() => props.onClick?.(props.name)}              // change undefined to the value this component uses
+        onPointerOver={() => props.onHover?.(props.name)}        // change undefined to the value this component uses
+        onPointerDown={() => props.onPointerDown?.(props.name)}  // change undefined to the value this component uses
+        onPointerUp={() => props.onPointerUp?.(props.name)}      // change undefined to the value this component uses
+    >
+        {/* component logic goes here */}
+        <h1>Header {props.name}</h1>
     </div>
 });
--- src/MyFirstInteractWidget/index.tsx
+++ src/MyFirstInteractWidget/index.tsx
@@ -1,26 +1,33 @@
-import { InteractComponent } from "@vintecc/interact";
+import { InteractComponent, GetBaseProperties, basePropsUiSettings, basePropsUiEvents } from "@vintecc/interact";
 import { MyFirstInteractWidget, IMyFirstInteractWidget } from "./component";
+import { Name, Version } from "./Version.json"
 
 const InteractComponentInstance = new InteractComponent<IMyFirstInteractWidget>(MyFirstInteractWidget);
-InteractComponentInstance.name = "MyFirstInteractWidget";
-InteractComponentInstance.version = "V1.0.0";
+InteractComponentInstance.name = Name;
+InteractComponentInstance.version = Version;
 InteractComponentInstance.category = "Basic";
+// if the component could be a parent and have child components set isCanvas to true
+// InteractComponentInstance.isCanvas = true;
 InteractComponentInstance.uiSettings = [
-   {
-    propName:"name",
-    type:"input",
-    default:"hi world",
-    category:"Basic",
-    name:"Name"
-    }
+    {
+        propName: "name",
+        type: "input",
+        default: ""
+    },
+
+    // there are some basic base properties on a component: "zIndex", "backgroundColor", "color", "fontSize", "width", "height" and "componentSpacing" (margin and padding).
+    // if these properties are not component-dependent, use the base properties.
+    ...GetBaseProperties([
+        "zIndex",
+        "backgroundColor",
+        "color",
+        "fontSize"
+    ]),
 ];
 InteractComponentInstance.uiEvents = [
-    {
-        propName:"onClick",
-        action:"event",
-        eventName:"",
-        name:"On click"
-    }
+    // basePropsUiEvents are standard events, e.g.: onClick, onHover, onPointerDown & onPointerUp
+    ...basePropsUiEvents,
+    
 ];
 
 export default InteractComponentInstance;
